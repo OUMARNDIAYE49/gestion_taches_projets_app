0274f9b8facdfdde4a5e10b1691e5e7ce89a88b7
OUMARNDIAYE49 <oumar.ndiaye4952@gmail.com>
2024-08-23 10:11:35 +0000

    Aprops

--- src/stores/taches.js
+++ src/stores/taches.js
@@ -1,24 +1,27 @@
-import { defineStore } from 'pinia';
+import { defineStore } from 'pinia'
 
 export const useTachesStore = defineStore('taches', {
-    state: () => ({
-        taches: [],
-    }),
-    actions: {
-        ajouterTache(tache) {
-            this.taches.push(tache);
-        },
-        supprimerTache(id) {
-            this.taches = this.taches.filter(tache => tache.id !== id);
-        },
-        obtenirTache(id) {
-            return this.taches.find(tache => tache.id === id);
-        },
-        mettreAJourTache(tacheMisAJour) {
-            const index = this.taches.findIndex(tache => tache.id === tacheMisAJour.id);
-            if (index !== -1) {
-                this.taches[index] = tacheMisAJour;
-            }
-        },
+  state: () => ({
+    taches: []
+  }),
+  actions: {
+    ajouterTache(tache) {
+      this.taches.push(tache)
     },
-});
+    supprimerTache(id) {
+      const index = this.taches.findIndex((tache) => tache.id === id)
+      if (index !== -1) {
+        this.taches.splice(index, 1)
+      }
+    },
+    obtenirTache(id) {
+      return this.taches.find((tache) => tache.id === id)
+    },
+    mettreAJourTache(tacheMisAJour) {
+      const index = this.taches.findIndex((tache) => tache.id === tacheMisAJour.id)
+      if (index !== -1) {
+        this.taches[index] = tacheMisAJour
+      }
+    }
+  }
+})
